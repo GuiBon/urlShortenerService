8e98406994360a79283505948b1ed740a26df465
Guillaume Bonin <guillaume.bonin@adevinta.com>
2024-10-21 20:50:21 +0200

    Add malwarescanner tests

--- domain/url_statistics.go
+++ domain/url_statistics.go
@@ -1,5 +1,6 @@
 package domain
 
+// URLStatistic represents an URL statistic with a shortened counter and an accessed counter
 type URLStatistic struct {
 	URL              string
 	ShortenedCounter int
